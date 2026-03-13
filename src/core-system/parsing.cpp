38d84ce2ab1f2998fd4d6cacff1348c489606ab6
QuantumCpp <jeremynobel16@gmail.com>
2026-03-12 19:18:44 -0500

    fix(parsing): fix short options block, add value type handling, fix positional fall-through

--- src/core-system/parsing.cpp
+++ src/core-system/parsing.cpp
@@ -1,8 +1,11 @@
 #include "../include/core-hpp/parsing.hpp"
+#include "command/command-implementation.hpp"
 #include "command/command-raw-metadata.hpp"
 #include "option/option-implementation.hpp"
 #include "option/option-raw-metadata.hpp"
 #include "token/token-raw-metadata.hpp"
+#include "../../include/error/error_hanlder.hpp"
+#include <cstddef>
 #include <string>
 #include <unordered_set>
 #include <vector>
@@ -12,76 +15,139 @@ namespace {
 }
 
 GroupToken parsing(const std::vector<Token>& token_raw){
-  GroupToken token_group;
-  std::unordered_set<std::string> see_argument;
+  GroupToken token_clasificated;
+  std::unordered_set<std::string> See_token;
+  
+  //Tratamiento y verificacion de existencia 
+  for(size_t pos = 0 ; pos < token_raw.size() ; pos++ ){
+    const auto& individual_token = token_raw[pos];
 
-  for(size_t i = 0 ; i < token_raw.size() ; i++){
-    const auto& object = token_raw[i];
-    
-    if(see_argument.contains(object.name)){
+    if(See_token.contains(individual_token.name) && individual_token.type == TypeToken::LITERAL){
       continue;
     }
 
-    //Tratamiento de opciones
-    if(object.type == TypeToken::OPTION_NOT_NORMALIZED){
-      const auto& option_data = GetOptionData(object.name);
-      //Verificar si un token de tipo opcion necesita valor asignado
-      // 1. en caso de tener se omite y se guarda como normalizado
-      // 2. en caso de tener un signo igual se separa y se guarda
-      // 3. en caso de no tener se da el siguiente token como su valor aninado si es un literal
-      // 4. en caso de que ninguna de estas se cumpla se retorna un error
-      if(option_data->data_type != TypeDataReceived::NONE){
-        auto its = object.name.find('=');
-        if(its != std::string::npos){
-          token_group.options.emplace_back(Token{
-              .type = TypeToken::OPTION_NORMALIZED,
-              .name = option_data->normalized_name,
-              .value = object.name.substr(its + 1),
-              });
-          continue;
-        }
-        if( i + i < token_raw.size() && token_raw[i + 1].type == TypeToken::LITERAL){
-          token_group.options.emplace_back(Token{
-              .type = TypeToken::OPTION_NORMALIZED,
-              .name = option_data->normalized_name,
-              .value = token_raw[i + 1].name,
-              });
-          see_argument.insert(token_raw[i+1].name);
-          continue;
-        }
-      }
-      //Verificar si la opcion es del tipo -abc o -abc=VALOR (opcions cortas agrupadas)
-      //1. si no tiene iguales simplemente se separa y se normalizada
-      // 2. en caso de tener iguales se separa y la ultima flag se robara el valor despues del signo
-      if(!object.name.starts_with("--") && object.name.starts_with('-') && object.name.size() > 2){
-        auto its = object.name.find("=");
-        //Caso 1 :
-        if(its == std::string::npos){
-          for(const auto& element : object.name){
-            if(element == '-'){
+    //Comprobar si existe un comando y si es valido :
+    if(individual_token.type == TypeToken::COMMAND){
+      const auto& data_token = GetCommandData(individual_token.name);
+      if(data_token == nullptr){
+        COMMAND_NOT_FOUND(individual_token.name);
+        return {};
+      };
+       token_clasificated.command = Token{
+          .type = TypeToken::COMMAND,
+          .name = individual_token.name,
+          .value = "",
+        };
+      continue;
+    }
+    
+    //Comprobar si una opcion existe
+    if(individual_token.type == TypeToken::OPTION_NOT_NORMALIZED){
+      //opciones largas
+      if(individual_token.name.starts_with("--")){
+        const auto equal_pos = individual_token.name.find('=');
+        //Opciones largas sin signo de igual 
+        if(equal_pos == std::string::npos){
+          const auto& data_token = GetOptionData(individual_token.name);
+          if(data_token == nullptr){
+            OPTION_NOT_FOUND(individual_token.name);
+            return {};
+          }
+          //Para opciones largas sin igual pero que requieren valor 
+          if(data_token->data_type != TypeDataReceived::NONE){
+            if(pos + 1 < token_raw.size() && token_raw[pos + 1].type == TypeToken::LITERAL){
+              const auto& next_token = token_raw[pos + 1];
+              token_clasificated.options.emplace_back(Token{
+                  .type = TypeToken::OPTION_NORMALIZED,
+                  .name = data_token->normalized_name,
+                  .value = next_token.name,
+                  });
+              See_token.insert(next_token.name);
               continue;
             }
-            const auto normalized_token = GetOptionData(std::string("-") + element);
-            token_group.options.emplace_back(Token{
-                .type = TypeToken::OPTION_NORMALIZED,
-                .name = normalized_token->normalized_name,
-                .value = "",
-                });
+            else{
+              OPTION_NEED_VALUE(individual_token.name,data_token->data_type);
+              return {};
+            }
           }
+          token_clasificated.options.emplace_back(Token{
+              .type = TypeToken::OPTION_NORMALIZED,
+              .name = data_token->normalized_name,
+              .value = "",
+              });
           continue;
         }
-        //Caso 2:
-
+        else{        
+          //Para opciones largas las cuales tienen un signo de igual 
+          const auto& data_token = GetOptionData(individual_token.name.substr(0,equal_pos));
+          if(data_token == nullptr){
+            OPTION_NOT_FOUND(individual_token.name);
+            return {};
+          }
+          token_clasificated.options.emplace_back(Token{
+            .type = TypeToken::OPTION_NORMALIZED,
+            .name = data_token->normalized_name,
+            .value = individual_token.name.substr(equal_pos + 1),
+            });
+          continue;
+        }
       }
+      //Para opciones cortas con o sin igual o agrupas con y sin igual
+      for(size_t i = 0 ; i < individual_token.name.size() ; i ++){
+        if(individual_token.name[i] == '-'){
+          continue;
+        }
+        const std::string flag = {'-', individual_token.name[i]};
+        const auto& data_token = GetOptionData(flag);
+        if(data_token == nullptr){
+          OPTION_NOT_FOUND(flag);
+          return {};
+        }
+        if(data_token->data_type != TypeDataReceived::NONE){
+          if(i + 1 < individual_token.name.size() && individual_token.name[i + 1] == '='){
+            const auto& equal_pos = individual_token.name.find('=');
+            if(equal_pos != std::string::npos){
+              token_clasificated.options.emplace_back(Token{
+                 .type = TypeToken::OPTION_NORMALIZED,
+                 .name = data_token->normalized_name,
+                 .value = individual_token.name.substr(equal_pos+1),
+                 });
+              break;
+            }
+          }
+          //Para caso especial de -o que requiera valor (para funciones agrupadas mandara error si no hay valor y se necesita)
+          else if(i + 1 >= individual_token.name.size() && pos + 1 < token_raw.size() 
+              && token_raw[pos + 1].type == TypeToken::LITERAL){
+             token_clasificated.options.emplace_back(Token{
+                 .type = TypeToken::OPTION_NORMALIZED,
+                 .name = data_token->normalized_name,
+                 .value = token_raw[pos + 1].name,
+                 });
+             See_token.insert(token_raw[pos + 1].name);
+             break;
+          }
+          else{
+            OPTION_NEED_VALUE(flag, data_token->data_type);
+            return {};
+          }
+        }
 
+        token_clasificated.options.emplace_back(Token{
+            .type = TypeToken::OPTION_NORMALIZED,
+            .name = data_token->normalized_name,
+            .value = "",
+            });
+        continue;
+      }
+      continue;
     }
 
-    //Verificar la existencia de un comando 
-    if(object.type == TypeToken::COMMAND){
-
-    }
+    token_clasificated.positional.emplace_back(Token{
+        .type = TypeToken::POSITIONAL,
+        .name = individual_token.name,
+        .value = "",
+        });
 
   }
-
-  return token_group;
+  return token_clasificated;
 }
